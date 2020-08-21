3bcbbd0a92e439e50000d21718c0f2d3e6d1dec5
alagrin <grinny@icloud.com>
2020-08-21 15:15:02 -0700

    Updates

---
+++ precache-manifest.634eb65d040cb7d030b743bdda57cdab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9720716f8cecfe268844d451d36ca0",
+    "url": "/portfolio-updated/index.html"
+  },
+  {
+    "revision": "3a322500a96f8d983f14",
+    "url": "/portfolio-updated/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1da69d766a630181d11e",
+    "url": "/portfolio-updated/static/js/2.2fcec213.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio-updated/static/js/2.2fcec213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a322500a96f8d983f14",
+    "url": "/portfolio-updated/static/js/main.becf5604.chunk.js"
+  },
+  {
+    "revision": "2167b335b46222731028",
+    "url": "/portfolio-updated/static/js/runtime-main.17777b72.js"
+  }
+]);
